fae1f28613af5d239a2ed20caffdcc99ea48a122
nanuThilak <nanuthilak2@gmail.com>
2025-10-04 18:36:52 +0530

    updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='mt-20'>About</div>
-  )
-}
+    <div className="mt-20 mx-5 flex flex-col gap-5">
+      <h1 className="text-2xl font-bold mb-5">About Us</h1>
+      <p className="">
+        At OG MOBILES, we’re more than just a mobile phone store — we’re your
+        go-to destination for the latest smartphones, accessories, and expert
+        advice. Whether you’re looking for the newest iPhone, the latest Samsung
+        Galaxy, or a reliable budget-friendly device, we’ve got you covered.
+      </p>
+      <h1 className="text-2xl font-bold mb-5">OUR MISSION</h1>
+      <p className="">
+        At OG MOBILES, our mission is to connect people with the right
+        technology by offering high-quality mobile devices, expert support, and
+        exceptional customer service — all at competitive prices. We strive to
+        be the most trusted mobile shop in [Your City/Region], where customers
+        can find not only the latest smartphones and accessories, but also
+        honest advice, fast repairs, and a friendly, helpful experience every
+        time they visit.
+      </p>
+      <h1 className="text-2xl font-bold mb-5">WHY CHOOSE US?</h1>
+      <p className="">
+        <ul type="square">
+          <li>. Competitive Prices</li>
+          <li>. Expert Support</li>
+          <li>. Fast & Reliable Repairs</li>
+          <li>. Wide Range of Products</li>
+        </ul>
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div className='mt-20'>Contact</div>
-  )
-}
+    <div className="my-20 mx-5">
+      <p className="text-center font-bold text-2xl">CONTACT US</p>
+      <h1 className="font-bold mb-5">GET IN TOUCH</h1>
+      <div>
+        Address: MobileTech Hub 123 Main Street Downtown Center, Metro City
+        10001 (Near City Mall, next to CoffeeZone)
+      </div>
+      <br />
+      <div>Opening Hours: Monday – Saturday: 10:00 AM – 8:00 PM</div>
+      <br />
+      <div>Sunday: Closed</div>
+      <br />
+      <div>Phone: +91 8974567989</div>
+      <br />
+      <div>Email: ogmobiles@mobiletechhub.com</div>
+      <br />
+      <div>Customer Support: supportog@mobiletechhub.com</div>
+      <br />
+<div className="border p-10 rounded-2xl">
+   <form action="" className="flex flex-col gap-5 w-[500px]">
+    <h1 className="text-2xl font-bold">Sending Us a Message</h1>
+    <label htmlFor="">Name</label>
+      <input type="Name" placeholder="Name" className="border-2 p-2" />
+      <label htmlFor="">Email</label>
+      <input type="email" placeholder="Email" className="border-2 p-2" />
 
-export default Contact
+  <label htmlFor="">Message</label>
+      <input type="email" placeholder="message" className="border-2 p-5" />
+      <button className="border rounded-2xl bg-white text-black w-25">Send</button>
+    </form>
+</div>
+ 
+    </div>
+  );
+};
+
+export default Contact;
